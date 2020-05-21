be9f21c7bdb9297351f02b504ebfc9b31cfa2904
kbjude <kbjude@gmail.com>
2020-05-21 16:54:09 +0300

    added the user variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        user = { username: params[:username], email: params[:email], password: params[:password] }
+        @user = User.new(user)
 
         if @user.save
             redirect_to new_user_path
