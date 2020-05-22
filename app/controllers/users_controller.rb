35a69d641922065f10c902d4afe910a799d5aa25
kbjude <kbjude@gmail.com>
2020-05-23 00:02:27 +0300

    Edit complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,16 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def update
+        @user = User.find(params[:id])
+        @user.update(use_params)
+        if @user.update(use_params)
+            redirect_to edit_user_path
+        else
+            render :edit
+        end
+    end
+
     private
 
     def use_params
