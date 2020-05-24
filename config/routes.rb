19987c1b8d911c50592778b3b57a8c9d1eaa3733
kbjude <kbjude@gmail.com>
2020-05-24 15:03:40 +0300

    routes fixed but broken edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
-  put "user/:id/edit" => "user#edit"
+  get "user/:id/edit" => "users#edit"
+  put "user/:id/edit" => "users#edit"
 end
