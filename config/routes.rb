6c151684bd09ca2f73280b44e8d794c1f8873484
kbjude <kbjude@gmail.com>
2020-05-25 13:24:46 +0300

    get on edit is working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
   get "user/:id/edit" => "users#edit"
-  put "user/:id/edit" => "users#edit"
+  put "users/:id/edit" => "users#edit"
 end
