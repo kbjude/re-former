1858198f4cc180b4fe970b1c2bed104f02018b72
kbjude <kbjude@gmail.com>
2020-05-26 12:49:00 +0300

    comments cleared

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create update]
   get "user/:id/edit" => "users#edit"
-  # put "user/:id" => "users#patch"
-
-  # resources :users, only: %i[new create edit update]
 end
